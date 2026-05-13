33b45a05b6de1f273838a3b0fcdfc27c259a663b
Gabriel Melo Ribeiro <meloribeiro0757@gmail.com>
2026-05-13 19:47:07 -0300

    feat: add project basic structure

---
+++ src/i18n/config.ts
@@ -0,0 +1,36 @@
+import i18n from "i18next";
+
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import { initReactI18next } from "react-i18next";
+
+import ptCommon from "../locales/pt/common.json";
+import enCommon from "../locales/en/common.json";
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      pt: {
+        "common": ptCommon,
+      },
+
+      en: {
+        "common": enCommon,
+      },
+    },
+
+    fallbackLng: "en",
+
+    detection: {
+      order: ["localStorage", "navigator"],
+      caches: ["localStorage"],
+    },
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
---
+++ src/routes/index.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+
+      <Route path="/home" element={<h1>Home</h1>} />
+
+      <Route path="/section1/pageA" element={<h1>Section 1 - Page A</h1>} />
+
+      <Route path="/section1/pageB" element={<h1>Section 1 - Page B</h1>} />
+
+      <Route path="/section2/pageA" element={<h1>Section 2 - Page A</h1>} />
+
+      <Route path="/section2/pageB" element={<h1>Section 2 - Page B</h1>} />
+    </Routes>
+  );
+}
