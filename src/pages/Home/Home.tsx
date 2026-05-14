96dfdd5bd8c52e700a7e038d13746920b617ce31
Gabriel Melo Ribeiro <meloribeiro0757@gmail.com>
2026-05-13 23:15:54 -0300

    feat: create the Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,25 @@
-export default function HomePage() {
-    return (
-        <h1>Home</h1>
-    )
+import { Box, Text } from "@chakra-ui/react";
+
+import { useTranslation } from "react-i18next";
+
+import { Layout } from "@/components/Layout/Layout";
+
+import {
+  containerStyles,
+  descriptionStyles,
+  titleStyles,
+} from "./styles";
+
+export function Home() {
+  const { t } = useTranslation("home");
+
+  return (
+    <Layout>
+      <Box {...containerStyles}>
+        <Text {...titleStyles}>{t("title")}</Text>
+
+        <Text {...descriptionStyles}>{t("description")}</Text>
+      </Box>
+    </Layout>
+  );
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,15 @@
+export const containerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 4,
+} as const;
+
+export const titleStyles = {
+  fontSize: "4xl",
+  fontWeight: "bold",
+} as const;
+
+export const descriptionStyles = {
+  fontSize: "lg",
+  color: "gray.600",
+} as const;
