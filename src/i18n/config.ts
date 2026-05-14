c839435d5f116e6d4a4b560e6f3ab5fcb41e990c
Gabriel Melo Ribeiro <meloribeiro0757@gmail.com>
2026-05-13 23:46:44 -0300

    feat: add support to i18n to pageA from section2

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -10,6 +10,8 @@ import ptHome from "../locales/pt/Home/home.json";
 import enHome from "../locales/en/Home/home.json";
 import ptSection1 from "../locales/pt/Sections/section1.json";
 import enSection1 from "../locales/en/Sections/section1.json";
+import ptSection2 from "../locales/pt/Sections/section2.json";
+import enSection2 from "../locales/en/Sections/section2.json";
 
 i18n
   .use(LanguageDetector)
@@ -20,12 +22,14 @@ i18n
         "common": ptCommon,
         "home": ptHome,
         "sections/section1": ptSection1,
+        "sections/section2": ptSection2,
       },
 
       en: {
         "common": enCommon,
         "home": enHome,
         "sections/section1": enSection1,
+        "sections/section2": enSection2,
       },
     },
 
