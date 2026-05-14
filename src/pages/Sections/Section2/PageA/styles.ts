9c11bbff33db512a5d7beb4b10173429e2077b11
Gabriel Melo Ribeiro <meloribeiro0757@gmail.com>
2026-05-13 23:49:16 -0300

    feat: create pageA page in section2

---
+++ src/pages/Sections/Section2/PageA/styles.ts
@@ -0,0 +1,16 @@
+export const containerStyles = {
+  display: "flex",
+  flexDirection: "column",
+  gap: 4,
+} as const;
+
+export const titleStyles = {
+  fontSize: "3xl",
+  fontWeight: "bold",
+} as const;
+
+export const descriptionStyles = {
+  fontSize: "lg",
+  color: "gray.600",
+  lineHeight: "1.8",
+} as const;
